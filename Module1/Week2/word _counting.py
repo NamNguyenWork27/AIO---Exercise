1e2992b84d6c75daefe88ff3de225b85c32466d9
NamNguyenWork27 <nghainamwork@gmail.com>
2024-06-17 14:52:40 +0000

    Exercise week2

---
+++ Module1/Week2/word _counting.py
@@ -0,0 +1,16 @@
+if __name__ == "__main__":
+    with open('/workspaces/AIO---Exercise/Module1/Week2/P1_data.txt', 'r') as f:
+        document = f.read()
+
+    words = document.split()
+
+    counter = {}
+    for word in words:
+        if word in counter:
+            counter[word] += 1
+        else:
+            counter[word] = 1
+
+    print(counter)
+
+
